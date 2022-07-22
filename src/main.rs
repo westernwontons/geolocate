85e9759eb3270da2121a963a000d19affc393b26
Nagy Botond <botondnagy98@icloud.com>
2022-07-22 10:36:29 +0300

    Better error handling with a useful error message that likely should never occur. The underlying error was returned as is, but it contained sensitive information. Now only the source of the error is returned.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ use crate::location::get_location::ip_points_to;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  println!("{}", ip_points_to().await?);
-  Ok(())
+  match ip_points_to().await {
+    Ok(ip_points_to) => {
+      println!("{}", ip_points_to);
+      Ok(())
+    }
+    Err(err) => {
+      eprintln!("{}", err.source().expect(
+        "Failed to print source of error. This is likely a bug. Report it at https://github.com/westernwontons/geolocate/issues"
+        ));
+      Ok(())
+    }
+  }
 }
