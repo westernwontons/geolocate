328faad4e870307b35abdcaa960436cdbf8592dd
Nagy Botond <75550932+westernwontons@users.noreply.github.com>
2023-02-22 19:16:27 +0200

    slimmed down on tokio features

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ mod geolocate_lib;
 
 use geolocate_lib::run::run;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    run().await
+fn main() -> anyhow::Result<()> {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_io()
+        .enable_time()
+        .build()?;
+
+    rt.block_on(async { run().await })
 }
